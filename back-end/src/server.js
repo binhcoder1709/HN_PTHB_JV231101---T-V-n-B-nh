c8234b92bcfab50b3582959525148f1648ce052d
binhcoder1709 <hn6006077@gmail.com>
2024-03-11 10:19:14 +0700

    project

---
+++ back-end/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors'
+const app = express();
+
+app.use(cors());
+
+const PORT = 3000;
+app.listen(PORT, ()=>
+{
+    console.log('Server is running with port: ', PORT);
+})
